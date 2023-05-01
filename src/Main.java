9549d1a22d6beb74ff6a623b61a44afd8faf99fd
Cylon_Neto_Learning <cylonn@ya.ru>
2023-05-01 14:42:24 +0500

    task1 rebuilded with methods

--- src/Main.java
+++ src/Main.java
@@ -5,27 +5,37 @@ import java.util.Date;
 
 public class Main {
     public static String ROOT = "D://IO_Games";
+    public static StringBuilder temp = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
-        StringBuilder temp = new StringBuilder();
         String[] dirPathsArray = new String[]{ROOT, ROOT + "/src", ROOT + "/res", ROOT + "/savegames", ROOT + "/temp",
                 ROOT + "/src/main", ROOT + "/src/test", ROOT + "/res/drawables", ROOT + "/res/vectors", ROOT + "/res/icons"};
         String[] filePathsArray = new String[]{ROOT + "/src/main/Main.java", ROOT + "/src/main/Utils.java",
                 ROOT + "/temp/temp.txt"};
-        for (int i = 0; i < dirPathsArray.length; i++) {
-            File dir = new File(dirPathsArray[i]);
+        dirAdd(dirPathsArray);
+        fileAdd(filePathsArray);
+        FileWriter fw = new FileWriter(ROOT + "/temp/temp.txt", true);
+        fw.write(temp.toString());
+        fw.close();
+
+    }
+    public static void dirAdd(String[] dirPaths){
+        for (int i = 0; i < dirPaths.length; i++) {
+            File dir = new File(dirPaths[i]);
             if (dir.mkdir()) {
-               
+
                 temp.append("At " + new Date() + " was created dir " + dir + "\n");
             } else {
                 temp.append(new Date() + " Cannot create directory " + dir + "  – File exists \n");
             }
         }
-        for (int i = 0; i < filePathsArray.length; i++) {
-            File myFile = new File(filePathsArray[i]);
+
+    }
+    public static void fileAdd(String[] filePaths){
+         for (int i = 0; i < filePaths.length; i++) {
+            File myFile = new File(filePaths[i]);
             try {
                 if (myFile.createNewFile()) {
-
                     temp.append("At " + new Date() + " was created file " + myFile + "\n");
                 } else {
                     temp.append(new Date() + " Cannot create directory " + myFile + "  – File exists \n");
@@ -35,9 +45,8 @@ public class Main {
                 temp = temp.append(new Date() + ex.getMessage() + "\n");
             }
         }
-        FileWriter fw = new FileWriter(ROOT + "/temp/temp.txt", true);
-        fw.write(temp.toString());
-        fw.close();
 
     }
 }
+
+
